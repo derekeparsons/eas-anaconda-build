dfa96cf2b4a50668a3b220dcf0a2db51c7efde5b
Derek Parsons <dep77@cornell.edu>
2020-11-03 08:44:02 -0500

    Merge adm and adm2, added python-wget and cfgrib to adm-dev

--- create-adm.sh
+++ create-adm.sh
@@ -2,4 +2,4 @@ conda create --name adm python=3.7 -y
 source activate adm
 conda install numpy scipy matplotlib netcdf4 cartopy xarray -y
 conda install ipython jupyter -y
-conda install -c conda-forge metpy siphon -y
+conda install -c conda-forge metpy siphon python-wget cfgrib -y
--- create-adm2.sh
+++
@@ -1,5 +0,0 @@
-conda create --name adm2 python=3.7 -y
-source activate adm2
-conda install numpy scipy matplotlib netcdf4 cartopy xarray -y
-conda install ipython jupyter -y
-conda install -c conda-forge  python-wget cfgrib metpy siphon -y
