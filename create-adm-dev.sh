52c8f7c9545aaebcd04751be8cced526dd78ab6c
Derek Parsons <dep77@cornell.edu>
2020-12-04 16:05:40 -0500

    adm/adm-dev:  pinned matplotlib to 3.3.3, added notes

--- create-adm-dev.sh
+++ create-adm-dev.sh
@@ -1,5 +1,26 @@
+# create-adm-dev.sh
+#
+#  Creates the adm/dynamics anaconda development environment
+#    (adm + jupytext for notebook development)
+#
+
+# Uses:
+#  
+#  EAS3411 - Atmospheric Thermodynamics and Hydrostatics Laboratory
+#  EAS3421 - Atmospheric Dynamics Discussion: Quantitative Visualization
+
+# Notes:
+#
+# Python is currently pinned to 3.7 due to limited availability of packages
+# in 3.8
+#
+# 20201204
+#   matplotlib=3.3.3 has been added to avoid matplotlib 3.3.2, which is buggy and
+#   the current default.  Once matplotlib 3.3.2 is no longer the default, this
+#   requirement can be removed
+
 conda create --name adm-dev python=3.7 -y
 source activate adm-dev
-conda install numpy scipy matplotlib netcdf4 cartopy xarray -y
+conda install numpy scipy netcdf4 cartopy xarray -y
 conda install ipython jupyter -y
-conda install -c conda-forge metpy siphon python-wget cfgrib jupytext -y
+conda install -c conda-forge metpy siphon python-wget cfgrib jupytext matplotlib=3.3.3 -y
